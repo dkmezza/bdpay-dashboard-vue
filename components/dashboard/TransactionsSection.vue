98934345359e0aa208dabaeb4b90f050090daa8c
David Mezza <d.mezza@zunocarbon.com>
2025-08-15 14:41:53 +0300

    :arrow_down: Added TransactionsSection component

---
+++ components/dashboard/TransactionsSection.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="mt-6">
+    <TransactionsTable />
+  </div>
+</template>
+
+<script setup lang="ts">
+import TransactionsTable from './TransactionsTable.vue'
+
+// This wrapper component will be used for additional
+// transactions-related functionality or layout
+</script>
